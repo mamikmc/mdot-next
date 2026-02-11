d19e8dd2baef217cbbfd83d63a956be62c05d8ab
倉持真美 <mami@kuramochimaminoMacBook-Air.local>
2026-02-11 22:42:22 +0900

    ヘッダー、フッターをコンポーネント化

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,49 +1,26 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
-  title: "Mdot -nextの練習",
-  description: "練習用に立ち上げました。",
+  title: "練習用サイト",
+  description: "Next.jsの学習用",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body>
-        {/* ここから追加 */}
-        <header className="bg-blue-600 text-white p-4">
-          <nav className="flex gap-6 items-center">
-            <h1 className="text-2xl font-bold">mdot-next: 練習サイト</h1>
-            <Link href="/" className="hover:underline">
-              ホーム
-            </Link>
-            <Link href="/about" className="hover:underline">
-              About
-            </Link>
-          </nav>
-        </header>
-        <main className="min-h-screen p-12">{children}</main>
+        <Header />
 
-        <footer className="bg-gray-500 text-white p-4 text-center">
-          <p>フッター</p>
-        </footer>
-        {/* ここまで追加 */}
+        <main className="min-h-screen p-8">{children}</main>
+
+        <Footer />
       </body>
     </html>
   );
